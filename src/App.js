a09ff179de28099cc8681e108ab4b632a44bc930
Ryan O’Boyle <durbanpoison@protonmail.com>
2022-09-14 15:33:03 -0700

    fixed margin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // @ts-nocheck
 import logo from './logo.png';
 import './App.css';
-import LinkerContainer from './components/linkerContainer';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
   return (
@@ -9,7 +11,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 				<h1>DurbanPoisonPew</h1>
-				<LinkerContainer />
+				<Card style={{ }}>
+				<Card.Title style={{color: 'white', fontWeight:'bolder', fontSize: '50px'}}>Durban's Links</Card.Title>
+				<Button
+					href='https://odysee.com/@DurbanPoisonPew:9'
+					varriant='secondary' className='Link'>
+					Odysee
+				</Button>
+				<Button
+					href='https://defcad.com/users/durbanpoisonpew/'
+					varriant='secondary' className='Link'>
+					Defcad
+				</Button>
+				<Button href='https://github.com/DurbansPoison' variant='secondary' className='Link'>
+					Github
+				</Button>
+				<Button
+					href='https://www.instagram.com/durbanpoisonpew/'
+					variant='secondary' className='Link'>
+					Instagram
+				</Button>
+			</Card>
       </header>
 
     </div>
--- src/components/linkerContainer.js
+++ src/components/linkerContainer.js
@@ -1,34 +1,12 @@
 // @ts-nocheck
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 import '../App.css';
 function LinkerContainer() {
 	return (
-		<Container className='LinkerContainer' style={{alignItems: 'center'}}>
-			<Card style={{ width: '300px', height: '200px' }}>
-				<Card.Title style={{color: 'white', fontWeight:'bolder', fontSize: '50px'}}>Durban's Links</Card.Title>
-				<Button
-					href='https://odysee.com/@DurbanPoisonPew:9'
-					varriant='secondary' className='Link'>
-					Odysee
-				</Button>
-				<Button
-					href='https://defcad.com/users/durbanpoisonpew/'
-					varriant='secondary' className='Link'>
-					Defcad
-				</Button>
-				<Button href='https://github.com/DurbansPoison' variant='secondary' className='Link'>
-					Github
-				</Button>
-				<Button
-					href='https://www.instagram.com/durbanpoisonpew/'
-					variant='secondary' className='Link'>
-					Instagram
-				</Button>
-			</Card>
-		</Container>
+		<div className='LinkerContainer' style={{alignItems: 'center'}}>
+		
+		</div>
 	);
 }
 
